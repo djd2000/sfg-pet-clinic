9d95bd9db48989beece042f9def89332719224c7
david <davidd125@yahoo.com>
2019-08-02 16:27:49 +0100

    Updated OwnerController and OwnerIndex. Closes 15

--- sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,27 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.sfgpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-	@RequestMapping({"", "/", "/index", "/index.html" })
-	public String listVets() {
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
+
+	private final OwnerService ownerService;
+
+	@RequestMapping({ "", "/", "/index", "/index.html" })
+	public String listVets(Model model) {
+		model.addAttribute("owners", ownerService.findAll());
+
 		return "owners/index";
 	}
+
 }
