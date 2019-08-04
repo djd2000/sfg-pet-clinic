200e367089b326eca0372d2a9b079a9cca88fbc6
david <davidd125@yahoo.com>
2019-08-04 09:14:36 +0100

    Refactored MapImpl to abstract ID generation. Closes #22

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,12 +1,24 @@
 package guru.springframework.sfgpetclinic.services.map;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public abstract class AbstractMapService<T, ID> {
-	protected Map<ID, T> map = new HashMap<>();
+import guru.springframework.sfgpetclinic.model.BaseEntity;
+/**
+ * 
+ * @author david
+ *
+ * @param <T>
+ * @param <ID>
+ * 
+ * #22
+ */
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+	protected Map<Long, T> map = new HashMap<>();
 
 	Set<T> findAll() {
 		return new HashSet<>(map.values());
@@ -16,8 +28,17 @@ public abstract class AbstractMapService<T, ID> {
 		return map.get(id);
 	}
 
-	T save(ID id, T object) {
-		return map.put(id, object);
+	T save(T object) {
+		if (object != null) {
+			if (object.getId() == null) {
+				object.setId(getNextId());
+			}
+			map.put(object.getId(), object);
+		} else {
+			throw new RuntimeException("Object cannot be null");
+		}
+
+		return object;
 	}
 
 	void deleteById(ID id) {
@@ -27,6 +48,12 @@ public abstract class AbstractMapService<T, ID> {
 
 	void delete(T object) {
 		map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+	}
+
+	private Long getNextId() {
+//      TODO try catch lesson 91
+		return map.isEmpty() ? 1L : Collections.max(map.keySet()) + 1;
+//		return Collections.max(map.keySet()) + 1; // exception
 	}
 
 }
