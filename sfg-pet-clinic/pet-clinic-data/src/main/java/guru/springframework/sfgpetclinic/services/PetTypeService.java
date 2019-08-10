07d43585d9c298e0d0634bfe006f185a7c2e2180
david <davidd125@yahoo.com>
2019-08-10 18:26:47 +0100

    Implemented Pet Type Service. Closes #28

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * 
+ * @author david
+ *
+ */
+public interface PetTypeService extends CrudService<PetType, Long>{
+
+
+
+}
