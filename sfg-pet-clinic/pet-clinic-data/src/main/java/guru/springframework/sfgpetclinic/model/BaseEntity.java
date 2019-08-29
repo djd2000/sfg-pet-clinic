79f3c1416ff8bc0ac3c18626aa3acdb2a8cc193c
david <davidd125@yahoo.com>
2019-08-29 22:03:41 +0100

    152. Spring pet clinic create base entity @MappedSuperClass

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -2,13 +2,25 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+/**
+ * 
+ * @author david
+ *
+ */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
@@ -18,8 +30,5 @@ public class BaseEntity implements Serializable {
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
-	
-	
 
 }
